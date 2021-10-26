b1274a70253593a046045814fa0ca77bc9aac6ea
makeit-mike <mike.isaac.jones@gmail.com>
2021-10-25 21:43:34 -0500

    adding filtering

--- SolutionExplorer/Program.fs
+++ SolutionExplorer/Program.fs
@@ -2,6 +2,7 @@
 open System
 open System.IO
 open System.Diagnostics
+open System.Linq
 
 // BLOCK: BEGIN GLOBALS
 let folderPath = "C:\\Users\\mikei\\source\\repos\\"
@@ -14,6 +15,9 @@ let msg_GetSolutionIndex = "\n\nType the number of the Solution you would like t
 let msg_UnableToParseEx = "Unable to parse to an int. Please try again. . .\n"
 
 // BLOCK: BEGIN FUNCTIONS
+
+let readLine = Console.ReadLine()
+
 let log (x: string) = Console.WriteLine x
 
 let isNull (x: string) = match box obj with | null -> true | _ -> false 
@@ -24,13 +28,18 @@ let toTestedInt (x: string) =
 let fileName (fullPath: string) =
     fullPath.Substring(fullPath.LastIndexOf('\\') + 1)
 
-let rec getProperFileIndex(x: int): int =
-    let input = toTestedInt (Console.ReadLine())
+let rec getIntInput(x: int): int =
+    let inputStr = readLine
+    let input = toTestedInt inputStr
     if (input = -1 || input >= x) then
         log msg_UnableToParseEx
-        getProperFileIndex(x)
+        getIntInput(x)
     else
         input
+
+let rec getIntInput_OptionalReturn(input: int, defaultReturn: int): int =
+    let input = toTestedInt (Console.ReadLine())
+    if (input = -1 || input >= input) then defaultReturn else input
 
 let getFullFilePaths(folderPath: string, fileType: string) = 
      Directory.GetFiles(folderPath, fileType, SearchOption.AllDirectories) 
@@ -46,13 +55,26 @@ let main argv =
 
         let fullPaths = getFullFilePaths(folderPath, "*.sln")
 
-        fullPaths 
-            |> List.iteri (fun i f -> log $"{i}: {fileName f}")
+        fullPaths |> List.iteri (fun i f -> log $"{i}: {fileName f}")
+
+        log "\nSearch by index (0) or by string (1) : Defualt 0."
+
+        let searchByString = getIntInput_OptionalReturn(1, 0) = 1; // (Input, DefualtReturnValue)
+
+        if (searchByString) then
+            log "What filter would you like to apply?"
+            let filterBy = Console.ReadLine().ToLower()
+            fullPaths 
+                |> Seq.where (fun x-> x.ToLower().Contains filterBy)
+                |> Seq.sort
+                |> Seq.toList
+                |> List.iteri (fun i f -> log $"{i}: {fileName f}")
 
         log msg_GetSolutionIndex
-
-        let _ = Process.Start(defaultApp, fullPaths.[getProperFileIndex(fullPaths.Length)])
+        let _ = Process.Start(defaultApp, fullPaths.[getIntInput(fullPaths.Length)])
         _APP_SUCCESS
     with ex ->
         printfn "%A" ex 
+        Console.WriteLine("Press enter to close this window.")
+        Console.ReadLine()
         _APP_FAILURE 
